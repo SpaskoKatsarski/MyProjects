0c255ee735b0dcb13468e8aaaf34eabe076a9f4a
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-11-12 16:32:06 +0200

    Updated

--- single-page-app-demo/demo/src/app.js
+++
@@ -1,9 +0,0 @@
-import { checkUserNav } from './auth.js';
-import { showCatalogView } from './catalog.js';
-import './details.js';
-import './login.js';
-
-checkUserNav();
-
-// Start application in catalog view
-showCatalogView();
