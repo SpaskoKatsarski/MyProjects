7545cc459b1faf75564a7a7cdd6e91b3ad8491cd
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-11-29 21:51:30 +0200

    Adding search functionality

--- music-app/src/views/searchView.js
+++ music-app/src/views/searchView.js
@@ -1,64 +1,57 @@
-import { html } from "../lib.js";
-import { getAlbumsByQuery } from "../api/data.js"
-import { detailsTemp } from "./detailsView.js";
-import { getUserData } from "../util.js";
+import { html, nothing } from "../lib.js";
+import { getAlbumsByQuery } from "../api/data.js";
+
+const searchTemp = (isClicked, handler, albums, hasUser) => html`
+    <section id="searchPage">
+        <h1>Search by Name</h1>
+    
+        <div class="search">
+            <input id="search-input" type="text" name="search" placeholder="Enter desired albums's name">
+            <button @click=${handler} class="button-list">Search</button>
+        </div>
+    
+        <h2>Results:</h2>
+        <div class="search-result">
+    ${isClicked ? 
+        albums.length > 0 ? html`
+                ${albums.map(a => createAlbumCard(a, hasUser))}
+            ` : 
+        html`<p class="no-result">No result.</p>`
+    : nothing}
+    </div>
+    </section>`;
+
+const createAlbumCard = (album, hasUser) => html`
+    <div class="card-box">
+        <img src=${album.imgUrl}>
+        <div>
+            <div class="text-center">
+                <p class="name">Name: ${album.name}</p>
+                <p class="artist">Artist: ${album.artist}</p>
+                <p class="genre">Genre: ${album.genre}</p>
+                <p class="price">Price: $${album.price}</p>
+                <p class="date">Release Date: ${album.releaseDate}</p>
+            </div>
+            ${hasUser ? html`
+                <div class="btn-group">
+                    <a href="/catalog/${album._id}" id="details">Details</a>
+                </div>
+            ` : nothing}
+        </div>
+    </div>
+`
 
 export function showSearch(ctx) {
-    ctx.render(searchTemp());
-}
+    ctx.render(searchTemp(false, onSearch));
 
-const searchTemp = () => html`
-    <section id="searchPage">
-        <h1>Search by Name</h1>
-    
-        <div class="search">
-            <input id="search-input" type="text" name="search" placeholder="Enter desired albums's name">
-            <button @click=${onSearch} class="button-list">Search</button>
-        </div>
-    
-        <h2>Results:</h2>
-    
-    </section>
-    `;
+    async function onSearch(e) {
+        const query = document.getElementById('search-input').value;
 
-const resultTemp = (albums, hasMatches, ownerId, onDelete) => html`
-    <section id="searchPage">
-        <h1>Search by Name</h1>
-    
-        <div class="search">
-            <input id="search-input" type="text" name="search" placeholder="Enter desired albums's name">
-            <button @click=${onSearch} class="button-list">Search</button>
-        </div>
-    
-        <h2>Results:</h2>
-    
-        ${hasMatches ? html`
-        <div class="search-result">
-            ${albums.map(a => detailsTemp(a, ownerId === a._ownerId, onDelete))}
-            <div>
-                `: html`
-                <div class="search-result">
-                    <p class="no-result">No result.</p>
-                    <div></div>
-                    `}
-    </section>
-    `;
+        if (!query) {
+            return alert('Enter text!');
+        }
 
-async function onSearch() {
-    const albums = await getAlbumsByQuery(document.getElementById('search-input').value);
-    const hasMatches = albums.length > 0;
-    const ownerId = context.user._id;
-
-    ctx.render(resultTemp(albums, hasMatches, ownerId, onDelete))
-}
-
-async function onDelete() {
-    const option = confirm('Are you sure that you want to delete this item?');
-    const user = await getUserData();
-    debugger;
-
-    if (option) {
-        await deleteAlbum(album._id);
-        context.page.redirect('/catalog')
+        const albums = await getAlbumsByQuery(query);
+        ctx.render(searchTemp(true, onSearch, albums, !!ctx.user));
     }
 }
