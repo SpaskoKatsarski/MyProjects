eed868484773396ce928540c4f72f9cefb8ca2fe
Spasko Katsarski <patsokatsarski1@gmail.com>
2023-05-02 12:11:20 +0300

    Put all functions into one object and replaced the view functions with page redirection

--- Flamingo/src/views/homeView.js
+++ Flamingo/src/views/homeView.js
@@ -1,26 +1,16 @@
-import { html } from "../lib.js";
-
-import { showArea } from "./areaView.js";
-import { showInfo } from "./infoView.js";
-import { showLakeView } from "./lakeView.js";
+import { html, page } from "../lib.js";
 
 export function showHome(ctx) {
-    ctx.render(homeTemp(showInfoView, showAreaView, showLakeInfo))
-
-    function showInfoView() {
-        showInfo(ctx)
+    const viewFunctions = {
+        info: () => page('/info'),
+        areas: () => page('/areas'),
+        lake: () => page('/lake')
     }
 
-    function showAreaView() {
-        showArea(ctx)
-    }
-
-    function showLakeInfo() {
-        showLakeView(ctx)
-    }
+    ctx.render(homeTemp(viewFunctions))
 }
 
-const homeTemp = (showInfoView, showAreaView, showLakeInfo) => html`
+const homeTemp = (viewFunctions) => html`
     <section class="firstsection">
       <div class="box-main">
           <div class="firstHalf">
@@ -32,7 +22,7 @@ const homeTemp = (showInfoView, showAreaView, showLakeInfo) => html`
                 Розовото фламинго е птица от семейство Фламингови. Розовото фламинго живее край солени езера и морски лагуни. Храни се с планктон, червеи, миди, скариди, ларви на насекоми и други богати на каротиноиди храни. Благодарение на странната му извита човка, която влачи във водата, то може да прецежда и задържа микроорганизмите от плитките места.
               </p>
 
-              <button style="--clr:#FF44CC" @click="${showInfoView}" class="custom-btn"><span>Научи още</span><i></i></button>
+              <button style="--clr:#FF44CC" @click="${viewFunctions.info}" class="custom-btn"><span>Научи още</span><i></i></button>
 
           </div>
       </div>
@@ -48,7 +38,7 @@ const homeTemp = (showInfoView, showAreaView, showLakeInfo) => html`
                 В Европа се среща в южна Франция и южна Испания. В Африка птицата е най-разпространена в Мароко, южен Тунис и Кения. Среща се и в Афганистан и северозападна Индия. Може да се види и в България целогодишно. През 2022 година, август в Бургаска област са преброени около три хиляди броя.
               </p>
 
-              <button style="--clr:#39FF14" @click="${showAreaView}" class="custom-btn"><span>Разгледай</span><i></i></button>
+              <button style="--clr:#39FF14" @click="${viewFunctions.areas}" class="custom-btn"><span>Разгледай</span><i></i></button>
 
           </div>
       </div>
@@ -62,7 +52,7 @@ const homeTemp = (showInfoView, showAreaView, showLakeInfo) => html`
             Над 1600 розови фламингота откриха дом в Атанасовското езеро, съобщено от Българското дружество за защита на птиците. Това е максималната численост, която е преброявана по нашите географски ширини в последните 15 години.
           </p>
 
-          <button style="--clr:#0FF0FC" @click="${showLakeInfo}" class="custom-btn"><span>Разгледай</span><i></i></button>
+          <button style="--clr:#0FF0FC" @click="${viewFunctions.lake}" class="custom-btn"><span>Разгледай</span><i></i></button>
 
       </div>
 
