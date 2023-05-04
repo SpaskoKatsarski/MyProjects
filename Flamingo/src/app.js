198d3b2272af9537c66d618b5d94417fbf0686e4
Spasko Katsarski <patsokatsarski1@gmail.com>
2023-05-04 12:30:48 +0300

    Updated

--- Flamingo/src/app.js
+++ Flamingo/src/app.js
@@ -7,6 +7,13 @@ import { showLakeView } from './views/lakeView.js'
 
 document.querySelector('button').addEventListener('click', onSearch)
 
+const flamingoEl = document.getElementById('flamingo')
+const areasEl = document.getElementById('areas')
+const lakeEl = document.getElementById('lake')
+
+//TODO: add a class which changes the color of the selected menu
+flamingoEl.addEventListener('click', changeColorFlamingo)
+
 const mainElement = document.getElementById('main-content')
 
 page(decorateContext)
@@ -25,16 +32,25 @@ function decorateContext(ctx, next) {
     next()
 }
 
+function changeColorFlamingo() {
+    debugger;
+    //make it with class not with the property 'style'
+    document.getElementById('flamingoLi').style.color = 'red'
+
+    // areasEl.classList.remove('active')
+    // lakeEl.classList.remove('active')
+}
+
 function onSearch() {
     // Simulating a search functionality
     const inputText = document.getElementById('search').value
     document.getElementById('search').value = null
     
-    if (inputText.toLowerCase() === 'фламинго' 
-        || inputText.toLowerCase() === 'flamingo') {
+    if (inputText.toLowerCase().includes('фламинго')
+        || inputText.toLowerCase().includes('flamingo')) {
         page('/info')
-    } else if (inputText.toLowerCase() === 'територии' 
-                || inputText.toLowerCase() === 'areas') {
+    } else if (inputText.toLowerCase().includes('територии')
+                || inputText.toLowerCase().includes('areas')) {
         page('/areas')
     } else if (inputText.toLowerCase().includes('езеро')
                 || inputText.toLowerCase().includes('lake')) {
